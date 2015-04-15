31a548b14faddf8649248ef9e919ded2bfb24dc0
Julie Pan <ycp217@nyu.edu>
2015-04-15 10:31:37 -0400

    update README with link to actual project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # facedetect
+Tested OpenCV in Python for Bitcamp '15. Code works as it is. Include different Haarcascade files to detect different features.
+
+Overlay any face in a camera stream with `face.jpg`. Replace `face.jpg` with a image of your choice.
+
+Completed project could be found (here)[https://github.com/chelseavalentine/Stardazed].
--- main.py
+++ main.py
@@ -3,31 +3,42 @@ import cv2
 
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 cap = cv2.VideoCapture(0)
 
+CELEB_INDEX = 0
+
 # set celeb and find 
-celeb_img = cv2.imread('face.jpg')
+celeb_names = ['Jamie Chung', 'Kim Kardashian', 'Robert Downey Jr']
+filename = 'img' + str(CELEB_INDEX) + '.png'
+
+print filename
+
+celeb_img = cv2.imread('img0.png', -1)
+
+
 celeb_gray = cv2.cvtColor(celeb_img, cv2.COLOR_BGR2GRAY)
 celeb_faces = face_cascade.detectMultiScale(celeb_gray, 1.05, 5)
 
 x, y, w, h = celeb_faces[0]
 
+cap = cv2.VideoCapture(0)
+
 while(True):
     ret, img = cap.read()
+    
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2RGBA)
 
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY) 
     faces = face_cascade.detectMultiScale(gray_img, 1.05, 5)
 
     for (p,q,r,s) in faces:
         cv2.rectangle(img,(p,q),(p+r,q+s),(255,0,0),2)
         face_gray = gray_img[q:q+s, p:p+r] 
         face_color = img[q:q+s, p:p+r] 
-        eyes = eye_cascade.detectMultiScale(face_gray) 
-        for (ep,eq,er,es) in eyes:
-           cv2.rectangle(face_color,(ep,eq),(ep+er,eq+es), (0,255,0),2)
     celeb_img = cv2.resize(celeb_img,(r,s))
-    print celeb_img.shape
+    
+    
+
 
     img[q:q+s, p:p+r] = celeb_img[0:celeb_img.shape[0], 0:celeb_img.shape[1]]
     cv2.imshow("output", img)
